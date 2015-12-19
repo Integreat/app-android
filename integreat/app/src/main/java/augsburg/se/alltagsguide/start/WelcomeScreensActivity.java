3d0e27e14180ec749c369b59bd1e8ad1ee7abed7
ammyg <amadeus.gebauer@gmail.com>
2015-12-19 23:26:00 +0100

    Adopt structure of master branch, added language selection to welcome screens

---
+++ integreat/app/src/main/java/augsburg/se/alltagsguide/start/WelcomeScreensActivity.java
@@ -0,0 +1,12 @@
+package augsburg.se.alltagsguide.start;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amade on 13. Dez. 2015.
+ */
+public class WelcomeScreensActivity extends AppCompatActivity {
+
+
+
+}
